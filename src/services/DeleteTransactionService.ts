8a70f91bb3f13b08520db8c2a16773626f9713bb
Amanda Oliveira <amanda.bap.oliveira@gmail.com>
2020-08-04 02:05:49 -0300

    End challenge

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,6 +1,5 @@
 import { getCustomRepository } from 'typeorm';
 import AppError from '../errors/AppError';
-import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
